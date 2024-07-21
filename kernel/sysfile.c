8e46c44d97ea5da667c8f379589133f48abfe162
AmEl <amirelkanov0504@gmail.com>
2024-07-22 01:53:14 +0300

    Seems like working symlink structure

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -328,7 +328,54 @@ sys_open(void)
       return -1;
     }
     ilock(ip);
-    if(ip->type == T_DIR && omode != O_RDONLY){
+
+    if (!(omode & O_NOFOLLOW)) { // Если разрешено бегать рекурсивно по символическим ссылкам, бегаем
+      int rec_depth = 0;
+      while (ip->type == T_SYMLINK) {
+        if (++rec_depth > MAXRECDEPTH) {
+          iunlock(ip);
+          end_op();
+          return -1;
+        }
+
+        char relative_path[MAXPATH];
+        memset(relative_path, 0, MAXPATH);
+
+        // Читаем цель символической ссылки в relative_path, и если не смогли, то красиво завершаемся
+        if (readi(ip, 0, (uint64) relative_path, 0, MAXPATH) < 0) {
+          iunlock(ip);
+          end_op();
+          return -1;
+        }
+        iunlock(ip);
+
+        if (relative_path[0] == '/') { // Если, на самом деле, это абсолютный путь
+          safestrcpy(path, relative_path, MAXPATH);
+        } else {
+          // Срезаем путь до последнего слеша
+          int path_size_until_last_slash = strlen(path);
+          while (path_size_until_last_slash > 0 && path[path_size_until_last_slash - 1] != '/') path_size_until_last_slash--;
+                
+          // Если путь слишком длинный, возвращаем ошибку
+          if ((path_size_until_last_slash + strlen(relative_path) + 1) > MAXPATH) {
+            end_op();
+            return -1;
+          }
+
+          // Припысываем относительный путь к абсолютному
+          memmove(path + path_size_until_last_slash, relative_path, strlen(relative_path) + 1);
+        }
+
+        if ((ip = namei(path)) == 0) { // Если не нашли inode по такому пути
+          end_op();
+          return -1;
+        }
+
+        ilock(ip);
+      }
+    }
+
+    if(ip->type == T_DIR && omode != O_RDONLY && omode != O_NOFOLLOW) {
       iunlockput(ip);
       end_op();
       return -1;
@@ -401,7 +448,7 @@ uint64 sys_readlink(void) {
     return -1;
   }
   ilock(ip);
-  readi(ip, 0, (uint64) buff, 0, ip->size);
+  readi(ip, 1, (uint64) buff, 0, ip->size);
   iunlock(ip);
   end_op();
   return 0;
